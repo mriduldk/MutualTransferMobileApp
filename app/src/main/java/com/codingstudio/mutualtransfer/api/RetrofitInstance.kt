16047c964f771e1eef18f596354b455b56566d2e
Mridul Das <mridul.das.9706@gmail.com>
2024-09-30 20:38:50 +0530

    added Jwt auth in header

--- app/src/main/java/com/codingstudio/mutualtransfer/api/RetrofitInstance.kt
+++ app/src/main/java/com/codingstudio/mutualtransfer/api/RetrofitInstance.kt
@@ -1,8 +1,13 @@
 package com.codingstudio.mutualtransfer.api
 
+import android.util.Log
+import com.codingstudio.mutualtransfer.BuildConfig
 import com.codingstudio.mutualtransfer.utils.Constants
-import io.grpc.android.BuildConfig
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -11,13 +16,23 @@ import java.util.concurrent.TimeUnit
 class RetrofitInstance {
 
     companion object{
+
         private val retrofit by lazy {
             val logging = HttpLoggingInterceptor()
 
             logging.setLevel(HttpLoggingInterceptor.Level.BODY)
 
+            val interceptor = object : Interceptor {
+                override fun intercept(chain: Interceptor.Chain): Response {
+                    val request = chain.request().newBuilder().addHeader("Authorization", getAuthorizationToken()).build()
+                    return chain.proceed(request = request)
+                }
+            }
+
+
             val client = OkHttpClient.Builder()
                 .addInterceptor(logging)
+                .addInterceptor(interceptor)
                 .connectTimeout(5, TimeUnit.MINUTES)
                 .writeTimeout(5, TimeUnit.MINUTES)
                 .readTimeout(5, TimeUnit.MINUTES)
@@ -28,6 +43,26 @@ class RetrofitInstance {
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build()
+        }
+
+        private fun getAuthorizationToken() : String {
+
+            val millis = System.currentTimeMillis()
+            val seconds = millis / 1000
+            val iat = seconds
+
+            val jwt = Jwts.builder()
+                .claim("iss", "localhost")
+                .claim("iat", iat)
+                .claim("nbf", iat)
+                .claim("exp", iat + 60)
+                .claim("aud", "AccessToken")
+                .signWith(SignatureAlgorithm.HS512, BuildConfig.JWT_SECRET_KEY.toByteArray())
+                .compact()
+
+            Log.e("getAuthorizationToken", "getAuthorizationToken: $jwt")
+
+            return "Bearer $jwt"
         }
 
         val authAPI by lazy {
@@ -51,6 +86,9 @@ class RetrofitInstance {
         val blockAPI by lazy {
             retrofit.create(RetrofitBlockAPI::class.java)
         }
+
+
+
     }
 
 }
